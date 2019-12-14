c51ba48f6a4a860fc0274b7d28c0d1f27b0fb258
甩甩 <shuaicode@gmail.com>
2019-12-14 17:11:27 +0800

    fix address

--- restful/models/address.go
+++ restful/models/address.go
@@ -7,7 +7,9 @@ import (
 type Address struct {
 	gorm.Model
 	UserId     int64  `gorm:"column:user_id" json:"userId"`         // 用户id
-	Username   string `gorm:"column:username" json:"username"`      // 收件人姓名
+	Name       string `gorm:"column:name" json:"name"`              // 收件人姓名
+	Mobile     string `gorm:"column:address" json:"mobile"`         // 手机
+	Address    string `gorm:"column:address" json:"address"`        // 地区
 	StreetName string `gorm:"column:street_name" json:"streetName"` // 街道地址
 	IsDefault  bool   `gorm:"column:is_default" json:"isDefault"`   // 是否是默认地址
 }
