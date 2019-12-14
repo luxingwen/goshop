d156374e3400c8cbdcaa99cc726a3b0915a62bd0
甩甩 <shuaicode@gmail.com>
2019-12-14 15:41:34 +0800

    fix

---
+++ restful/models/category.go
@@ -0,0 +1,22 @@
+package models
+
+type Category struct {
+	gorm.Model
+	Title     string `gorm:"column:title" json:"title"`          // 分类名
+	SortOrder int    `gorm:"column:sort_order" json:"sortOrder"` // 排序
+	PicUrl    string `gorm:"column:pic_url" json:"picUrl"`       // 分类大封面
+	Type      int    `gorm:"column:type" json:"type"`            //类型
+	RelateId  int    `gorm:"column:relate_id" json:"relateId"`   // 关联id
+	ParentId  int    `gorm:"column:parent_id" json:"parent_id"`  // 父级id
+}
+
+// 返回的category 结构体
+type ResCategory struct {
+	Id        int            `json:"id"`
+	Title     string         `json:"title"`       // 分类名
+	SortOrder int            `json:"sortOrder"`   // 排序
+	PicUrl    string         `json:"picUrl"`      // 分类大封面
+	Type      int            `json:"type"`        // 类型
+	RelateId  int            `json:"relateId"`    // 关联id
+	Children  []*ResCategory `json:"ResCategory"` // 孩子
+}
