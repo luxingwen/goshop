349c5e71b909b3286f13f019ada84df380ad8474
甩甩 <shuaicode@gmail.com>
2019-12-14 17:39:43 +0800

    fix

--- restful/models/item.go
+++ restful/models/item.go
@@ -6,9 +6,12 @@ import (
 
 type Item struct {
 	gorm.Model
-	Title    string  `gorm:"column:title"`
-	Price    float64 `gorm:"column:price"`
-	Num      int64   `gorm:"column:num"`
-	LimitNum int64   `gorm:"column:limit_num"`
-	Status   int64   `gorm:"column:status"`
+	Title       string  `gorm:"column:title"`
+	Price       float64 `gorm:"column:price"`
+	OriginPrice float64 `gorm:"orginPrice"`
+	Num         int64   `gorm:"column:num"`       // 数量
+	LimitNum    int64   `gorm:"column:limit_num"` // 限制数量
+	Status      int64   `gorm:"column:status"`    // 状态
+	Desc        string  `gorm:"column:desc"`      // 描述
+	PicUrl      string  `gorm:"column:picUrl"`    // 图片地址
 }
